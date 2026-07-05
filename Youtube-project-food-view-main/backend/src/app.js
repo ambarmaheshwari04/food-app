887d2704f4cc01297ae27af5cac96a11507e9695
Ambar Maheshwari <ambarmaheshwari04@gmail.com>
2026-07-05 20:32:25 +0530

    Update app.js

--- Youtube-project-food-view-main/backend/src/app.js
+++ Youtube-project-food-view-main/backend/src/app.js
@@ -8,8 +8,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+origin: 'https://food-app-frontend-cilk.onrender.com',    credentials: true
 }));
 app.use(cookieParser());
 app.use(express.json());
