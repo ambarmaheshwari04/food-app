513487d085d8caaf851cd279f429cfde9d476420
Ambar Maheshwari <ambarmaheshwari04@gmail.com>
2026-07-05 18:02:01 +0530

    Update ReelFeed.jsx

--- Youtube-project-food-view-main/frontend/src/components/ReelFeed.jsx
+++ Youtube-project-food-view-main/frontend/src/components/ReelFeed.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef, useContext } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'react-router-dom';
 import axios from 'axios';
 import { AppContext } from '../routes/AppRoutes';
 import { toast } from 'react-toastify';
